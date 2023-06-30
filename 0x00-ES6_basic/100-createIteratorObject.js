ad03596c19a20bf922a642aaf6e68b1cf3a9ae9f
ESTHEREKANEM <estekanem1@gmail.com>
2023-06-30 21:00:27 +0000

    Advaanced task upload

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+	let allEmployees = [];
+	for (const item of Object.values(report.alllEmployees)) {
+		...allEmployees,
+		...items,
+		};
+}
+return allEmployees;
+}
